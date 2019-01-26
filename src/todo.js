13b999c1d6fc389063ad8899420540c9511485ee
Makito Katsube <katsubemakito@gmail.com>
2019-01-27 05:19:09 +0900

    implements Ajax

--- src/todo.js
+++ src/todo.js
@@ -7,35 +7,90 @@ class ToDo extends Component {
     super(props);
     this.state = {
       todo: [],
-      max_id: 0
+      isLoad: false,
+      isError: false
     };
 
     this.handleClick  = this.handleClick.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
   }
 
-  handleClick(value){
-    let id   = this.state.max_id;
-    let todo = this.state.todo;
-    todo.push({id: id+1, name:value});
+  componentWillMount(){
+    this.fetchAPI();
+  }
 
-    this.setState({
-        todo: todo,
-        max_id: id + 1
-    });
+  fetchAPI(){
+    fetch('http://localhost:3001/get')
+      .then( res => res.json() )
+      .then(
+        res => {
+          this.setState({
+            todo: res.data,
+            isLoad: true
+          });
+        },
+        res => {
+          this.setState({ isError: true });
+        },
+      );
+  }
+
+  saveAPI(value){
+    let method  = "POST";
+    let headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'};
+    let body    = "data=" + JSON.stringify(value);
+
+    fetch('http://localhost:3001/set', {method, headers, body})
+      .then( res => res.json() )
+      .then(
+        res => {
+          if(res.status === false){
+            this.setState({isError:true})
+          }
+          else{
+            this.fetchAPI();
+          }
+        },
+        res => {
+          this.setState({isError:true})
+        }
+      );
+  }
+
+  removeAPI(id){
+    fetch(`http://localhost:3001/remove/${id}`)
+      .then( res => res.json() )
+      .then(
+        res => {
+          if(res.status === false){
+            this.setState({isError:true})
+          }
+          else{
+            this.fetchAPI();
+          }
+        },
+        res => {
+          this.setState({ isError: true });
+        },
+      );
+  }
+
+  handleClick(value){
+    this.saveAPI({name:value});
   }
 
   handleRemove(e){
-    let cur  = this.state.todo;
-    let id   = Number( e.currentTarget.getAttribute("data-id") );
-    let todo = cur.filter( i => i.id !== id );
-
-    this.setState({
-      todo: todo
-    });
+    let id = Number( e.currentTarget.getAttribute("data-id") );
+    this.removeAPI(id);
   }
 
   render() {
+    if(this.state.isError){
+      return( <div>API Request Error</div> );
+    }
+    else if(!this.state.isLoad){
+      return( <div>...Loading now</div> );
+    }
     return(
       <div>
         <ToDoCreate onClick={this.handleClick} />
